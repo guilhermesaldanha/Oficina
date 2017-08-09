53d11eeac577d09651581ba3a97ed194f9f1b739
guilhermesaldanha <saldanha.guilherme1902@gmail.com>
2017-08-09 20:26:23 -0300

    Gotas 2.5 adicionado
    Gotas 2.5 com paralax quase perfeito e alterações de imagens

---
+++ Gotas 2.5/GDF/Assets/Script/Sun.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun : MonoBehaviour {
+
+    private Material currentMaterial;
+    public float speed;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Translate(Vector3.left * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag.Equals("BackTrigger"))
+        {
+            this.transform.position = new Vector3(15f, 1.76f, 0);
+        }
+    }
+}
